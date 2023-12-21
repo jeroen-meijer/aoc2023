d3bf5a535d873c8e6b481b54d53241a31322a747
Jeroen Meijer <jeroenfkmeijer@gmail.com>
2023-12-21 23:57:24 +0100

    feat: finish assignment 3

--- src/assignments/assignment_3.rs
+++ src/assignments/assignment_3.rs
@@ -1,10 +1,8 @@
-use super::prelude::*;
-use std::{
-    fmt::{write, Display},
-    ops::RangeInclusive,
-};
+use std::ops::RangeInclusive;
 
-const LOGGING_ENABLED: bool = false;
+use itertools::Itertools;
+
+use super::prelude::*;
 
 pub fn get_assignment() -> Assignment {
     Assignment::new(
@@ -27,51 +25,127 @@ pub fn get_assignment() -> Assignment {
 .664.598..",
             ),
             answer_example_day_1: Some(4361.into()),
-            answer_day_1: None,
-            example_input_day_2: None,
-            answer_example_day_2: None,
-            answer_day_2: None,
+            answer_day_1: Some(551094.into()),
+            example_input_day_2: Some(
+                "
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..",
+            ),
+            answer_example_day_2: Some(467835.into()),
+            answer_day_2: Some(80179647.into()),
         },
         // cspell: enable
     )
 }
 
-fn _run(data: &Vec<String>, _is_day_2: bool) -> Result<Option<Answer>, String> {
+fn _run(data: &Vec<String>, is_day_2: bool) -> Result<Option<Answer>, String> {
     let matrix = EngineMatrix::parse(data);
 
-    let mut p = false;
-    for SymbolEntry { x, y } in matrix.symbols {
-        let x = x as i64;
-        let y = y as i64;
-        let surrounding_coords = vec![
-            (x - 1, y - 1),
-            (x, y - 1),
-            (x + 1, y - 1), //
-            (x - 1, y),
-            (x, y),
-            (x + 1, y), //
-            (x - 1, y + 1),
-            (x, y + 1),
-            (x + 1, y + 1), //
-        ]
-        .into_iter()
-        .filter_map(|(x, y)| {
-            if x < 0i64 || y < 0i64 {
-                None
-            } else {
-                Some((x as u32, y as u32))
-            }
-        })
-        .collect::<Vec<_>>();
+    if !is_day_2 {
+        let numbers_surrounded_by_symbols = matrix
+            .symbols
+            .iter()
+            .flat_map(|SymbolEntry { x, y, character: _ }| {
+                let x = *x as i64;
+                let y = *y as i64;
+                let surrounding_coords = vec![
+                    (x - 1, y - 1),
+                    (x, y - 1),
+                    (x + 1, y - 1), //
+                    (x - 1, y),
+                    (x, y),
+                    (x + 1, y), //
+                    (x - 1, y + 1),
+                    (x, y + 1),
+                    (x + 1, y + 1), //
+                ]
+                .into_iter()
+                .filter_map(|(x, y)| {
+                    if x < 0i64 || y < 0i64 {
+                        None
+                    } else {
+                        Some((x as usize, y as usize))
+                    }
+                })
+                .collect::<Vec<_>>();
 
-        for (x, y) in surrounding_coords {
-            // Check if number is present in these coords.
-            // [Y] Save, then eventually sum and return.
-            // [N] Ignore.
-        }
+                surrounding_coords.into_iter().flat_map(|(x, y)| {
+                    matrix
+                        .numbers
+                        .iter()
+                        .filter(|e| e.y == y && e.x.contains(&x))
+                        .collect::<Vec<_>>()
+                })
+            })
+            .unique()
+            .map(|e| e.value);
+
+        let sum = numbers_surrounded_by_symbols.sum::<u32>();
+
+        Ok(Some(sum.into()))
+    } else {
+        let numbers_surrounded_by_stars_multiplied =
+            matrix.symbols.iter().filter(|e| e.character == '*').map(
+                |SymbolEntry { x, y, character: _ }| {
+                    let x = *x as i64;
+                    let y = *y as i64;
+                    let surrounding_coords = vec![
+                        (x - 1, y - 1),
+                        (x, y - 1),
+                        (x + 1, y - 1), //
+                        (x - 1, y),
+                        (x, y),
+                        (x + 1, y), //
+                        (x - 1, y + 1),
+                        (x, y + 1),
+                        (x + 1, y + 1), //
+                    ]
+                    .into_iter()
+                    .filter_map(|(x, y)| {
+                        if x < 0i64 || y < 0i64 {
+                            None
+                        } else {
+                            Some((x as usize, y as usize))
+                        }
+                    })
+                    .collect::<Vec<_>>();
+
+                    let numbers_surrounded = surrounding_coords
+                        .into_iter()
+                        .flat_map(|(x, y)| {
+                            matrix
+                                .numbers
+                                .iter()
+                                .filter(|e| e.y == y && e.x.contains(&x))
+                                .collect::<Vec<_>>()
+                        })
+                        .unique()
+                        .collect::<Vec<_>>();
+
+                    if numbers_surrounded.len() != 2 {
+                        0
+                    } else {
+                        vec![numbers_surrounded.first(), numbers_surrounded.last()]
+                            .iter()
+                            .map(|e| e.unwrap().value)
+                            .reduce(|a, b| a * b)
+                            .unwrap()
+                    }
+                },
+            );
+
+        let sum = numbers_surrounded_by_stars_multiplied.sum::<u32>();
+
+        Ok(Some(sum.into()))
     }
-
-    Ok(None)
 }
 
 struct EngineMatrix {
@@ -125,7 +199,11 @@ impl EngineMatrix {
                         acc.push(Entry::Number(next_entry));
                         acc
                     } else {
-                        acc.push(Entry::Symbol(SymbolEntry { x, y }));
+                        acc.push(Entry::Symbol(SymbolEntry {
+                            x,
+                            y,
+                            character: chr,
+                        }));
                         acc
                     }
                 })
@@ -145,7 +223,7 @@ impl EngineMatrix {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Hash)]
 struct NumberEntry {
     value: u32,
     x: RangeInclusive<usize>,
@@ -154,6 +232,7 @@ struct NumberEntry {
 
 #[derive(Debug)]
 struct SymbolEntry {
+    character: char,
     x: usize,
     y: usize,
 }
